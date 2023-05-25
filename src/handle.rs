a2411cec7cc92b5205ad9a031d4364ea9d018a39
jinser <cmdr.jv@gmail.com>
2023-05-25 18:05:26 +0800

    placeholder

---
+++ src/handle.rs
@@ -0,0 +1,105 @@
+use std::matches;
+
+use flowsnet_platform_sdk::write_error_log;
+use github_flows::{
+    get_octo, octocrab::models::events::payload::PullRequestEventAction, EventPayload, GithubLogin,
+};
+use ligab::Liga;
+use openai_flows::FlowsAccount;
+use regex::Regex;
+use serde_json::Value;
+
+use crate::review::get_review;
+
+pub async fn handle(
+    login: &GithubLogin,
+    owner: &str,
+    repo: &str,
+    liga: Liga,
+    account: FlowsAccount,
+    payload: EventPayload,
+) {
+    if let EventPayload::IssueCommentEvent(e) = payload {
+        let pr_url = if let Some(pr) = e.issue.pull_request {
+            pr.html_url
+        } else {
+            write_error_log!("not pr");
+            return;
+        };
+
+        let comment = e.comment.body.unwrap_or_default();
+        let octo = get_octo(login);
+
+        let re = Regex::new(r#"LigaAI#(\w{1,5}-\d+)"#).unwrap();
+        let captures = re.captures(&comment);
+
+        let issue_number = if let Some(cap) = captures {
+            cap.get(1).unwrap().as_str()
+        } else {
+            return;
+        };
+
+        let issue: Value = liga.issue().get_by_issue_number(issue_number);
+
+        let code = issue["code"].as_str().unwrap_or("-1");
+        if code != "0" {
+            // Error here
+            return;
+        }
+
+        let data = &issue["data"];
+        let project_id = &data["projectId"];
+        let issue_id = data["id"].as_u64().unwrap() as u32;
+        let description = data["data"]["description"].as_str().unwrap_or_default();
+
+        let title = e.issue.title;
+        let pull_number = e.issue.number;
+        let body = get_review(octo, owner, repo, &title, pull_number, account)
+            .await
+            .unwrap();
+
+        let data = serde_json::json!({
+            "description": format!("{}\n> ref: {}\n{}", description, pr_url, body),
+        });
+        let res: Value = liga.issue().update(data, issue_id);
+
+        let success = &res["data"]["success"].as_bool();
+        let number = e.issue.number;
+
+        if let Some(suc) = success {
+            if *suc {
+                let url =
+                    format!("https://ligai.cn/app/work/table?pid={project_id}&issueid={issue_id}");
+                let body = format!("Review sent!\nPlease visit [LigaAI]({url}) to check it out.");
+                _ = octo.issues(owner, repo).create_comment(number, body).await;
+            }
+
+            return;
+        }
+
+        _ = octo
+            .issues(owner, repo)
+            .create_comment(
+                number,
+                format!("failed...\n{:?}", serde_json::to_string(&res)),
+            )
+            .await;
+    } else if let EventPayload::PullRequestEvent(e) = payload {
+        if !matches!(e.action, PullRequestEventAction::Synchronize) {
+            write_error_log!("not synchronize action");
+            return;
+        }
+
+        let octo = get_octo(login);
+
+        if let Some(rp) = e.pull_request.repo {
+            if let Some(cp_url) = rp.compare_url {
+                let number = e.number;
+                _ = octo
+                    .issues(owner, repo)
+                    .create_comment(number, format!("compare_url: {}", cp_url))
+                    .await;
+            }
+        }
+    }
+}
---
+++ src/review.rs
@@ -0,0 +1,96 @@
+use flowsnet_platform_sdk::write_error_log;
+use github_flows::octocrab::Octocrab;
+use openai_flows::{
+    chat::{ChatModel, ChatOptions},
+    FlowsAccount, OpenAIFlows,
+};
+
+use crate::CHAR_SOFT_LIMIT;
+
+pub async fn get_review(
+    octo: &Octocrab,
+    owner: &str,
+    repo: &str,
+    title: &str,
+    pull_number: u64,
+    account: FlowsAccount,
+) -> Option<String> {
+    let pulls = octo.pulls(owner, repo);
+
+    let patch_as_text = pulls.get_patch(pull_number).await.unwrap();
+    let mut current_commit = String::new();
+    let mut commits: Vec<String> = Vec::new();
+    for line in patch_as_text.lines() {
+        if line.starts_with("From ") {
+            // Detected a new commit
+            if !current_commit.is_empty() {
+                // Store the previous commit
+                commits.push(current_commit.clone());
+            }
+            // Start a new commit
+            current_commit.clear();
+        }
+        // Append the line to the current commit if the current commit is less than CHAR_SOFT_LIMIT
+        if current_commit.len() < CHAR_SOFT_LIMIT {
+            current_commit.push_str(line);
+            current_commit.push('\n');
+        }
+    }
+    if !current_commit.is_empty() {
+        // Store the last commit
+        commits.push(current_commit.clone());
+    }
+
+    if commits.is_empty() {
+        write_error_log!("Cannot parse any commit from the patch file");
+        return None;
+    }
+
+    let mut of = OpenAIFlows::new();
+    of.set_flows_account(account);
+    of.set_retry_times(3);
+
+    let chat_id = format!("PR#{pull_number}");
+    let system = &format!("You are an experienced software developer. You will act as a reviewer for a GitHub Pull Request titled \"{}\".", title);
+    let mut reviews: Vec<String> = Vec::new();
+    let mut reviews_text = String::new();
+    for (_i, commit) in commits.iter().enumerate() {
+        let co = ChatOptions {
+            model: ChatModel::GPT35Turbo,
+            restart: true,
+            system_prompt: Some(system),
+        };
+        let question = "The following is a GitHub patch. Please summarize the key changes and identify potential problems. Start with the most important findings.\n\n".to_string() + commit;
+        if let Ok(r) = of.chat_completion(&chat_id, &question, &co).await {
+            if reviews_text.len() < CHAR_SOFT_LIMIT {
+                reviews_text.push_str("------\n");
+                reviews_text.push_str(&r.choice);
+                reviews_text.push('\n');
+            }
+            reviews.push(r.choice);
+        }
+    }
+
+    let mut resp = String::new();
+    resp.push_str("Hello, I am a [serverless review bot](https://github.com/flows-network/github-pr-summary/) on [flows.network](https://flows.network/). Here are my reviews of code commits in this PR.\n\n------\n\n");
+    if reviews.len() > 1 {
+        let co = ChatOptions {
+            model: ChatModel::GPT35Turbo,
+            restart: true,
+            system_prompt: Some(system),
+        };
+        let question = "Here is a set of summaries for software source code patches. Each summary starts with a ------ line. Please write an overall summary considering all the individual summary. Please present the potential issues and errors first, following by the most important findings, in your summary.\n\n".to_string() + &reviews_text;
+        if let Ok(r) = of.chat_completion(&chat_id, &question, &co).await {
+            write_error_log!("Got the overall summary");
+            resp.push_str(&r.choice);
+            resp.push_str("\n\n## Details\n\n");
+        }
+    }
+    for (i, review) in reviews.iter().enumerate() {
+        resp.push_str(&format!("### Commit {}\n", i + 1));
+        resp.push_str(review);
+        resp.push_str("\n\n");
+    }
+
+    Some(resp)
+}
